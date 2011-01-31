6002a16385f6c51b65fb4623eaf981bf9bc8b7fb
Bradford Hovinen <hovinen@gmail.com>
2011-01-31 16:59:28 +0100

    * Add specialiation of MatrixDomainSupport to GF2 in matrix-domain-gf2.{h|inl}  * Remove specialisation of MatrixDomain to GF2 from gf2.inl

---
+++ linbox/matrix/matrix-domain-gf2.h
@@ -0,0 +1,392 @@
+/* -*- mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+
+/* linbox/matrix/matrix-domain-gf2.h
+ * Copyright 2010 Bradford Hovinen
+ *
+ * Written by Bradford Hovinen <hovinen@gmail.com>
+ *
+ * ------------------------------------
+ *
+ * See COPYING for license information.
+ */
+
+#ifndef __MATRIX_MATRIX_DOMAIN_GF2_H
+#define __MATRIX_MATRIX_DOMAIN_GF2_H
+
+#include <iostream>
+#include <time.h>
+
+#include "linbox/field/gf2.h"
+#include "linbox/matrix/matrix-domain.h"
+#include "linbox/matrix/dense-zero-one.h"
+#include "linbox/matrix/submatrix.h"
+#include "linbox/vector/sparse-subvector-hybrid.h"
+
+namespace LinBox 
+{
+
+/** Matrix-vector and matrix-matrix arithmetic over GF2
+ *
+ * This class provides broad support for all combinations of matrix-
+ * and vector-types, but uses only naive algorithms and is therefore
+ * not tuned well for performance, especially for dense matrices.
+ */
+class MatrixDomainSupportGF2
+{
+protected:
+	const bool _zero;
+	const bool _one;
+	const bool _neg_one;
+
+	const GF2 &_F;
+	const VectorDomain<GF2> &_VD;
+
+	///
+	MatrixDomainSupportGF2 (const GF2 &F) : _F (F), _VD (F), _zero (false), _one (true), _neg_one (true) {}
+
+public:
+
+	template <class Vector1, class Matrix, class Vector2>
+	inline Vector2 &gemv (const bool &a, const Matrix &A, const Vector1 &x, const bool &b, Vector2 &y) const
+		{ return gemvSpecialized (a, A, x, b, y, typename MatrixTraits<Matrix>::MatrixCategory ()); }
+
+	template <class Matrix, class Vector>
+	inline Vector &trsv (const Matrix &A, Vector &x) const
+		{ return trsvSpecialized (A, x, typename MatrixTraits<Matrix>::MatrixCategory (), typename VectorTraits<Vector>::VectorCategory ()); }
+
+	template <class Matrix1, class Matrix2>
+	inline Matrix1 &copy (Matrix1 &B, const Matrix2 &A) const
+		{ return copySpecialized (B, A,
+					  typename MatrixTraits<Matrix1>::MatrixCategory (),
+					  typename MatrixTraits<Matrix2>::MatrixCategory ()); }
+
+	template <class Matrix1, class Matrix2>
+	inline bool areEqual (const Matrix1 &A, const Matrix2 &B) const
+		{ return areEqualSpecialized (B, A,
+					      typename MatrixTraits<Matrix1>::MatrixCategory (),
+					      typename MatrixTraits<Matrix2>::MatrixCategory ()); }
+
+	template <class Matrix>
+	inline bool isZero (const Matrix &A) const
+		{ return isZeroSpecialized (A, typename MatrixTraits<Matrix>::MatrixCategory ()); }
+
+	template <class Matrix>
+	inline Matrix &scal (Matrix &A, const bool &a) const
+		{ return scalSpecialized (A, a,
+					  typename MatrixTraits<Matrix>::MatrixCategory ()); }
+
+	template <class Matrix1, class Matrix2>
+	inline Matrix2 &axpy (const bool &a, const Matrix1 &A, Matrix2 &B) const
+		{ return axpySpecialized (a, A, B,
+					  typename MatrixTraits<Matrix1>::MatrixCategory (),
+					  typename MatrixTraits<Matrix2>::MatrixCategory ()); }
+
+	template <class Matrix1, class Matrix2, class Matrix3>
+	inline Matrix3 &gemm (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C) const
+		{ return gemmSpecialized (a, A, B, b, C,
+					  typename MatrixTraits<Matrix1>::MatrixCategory (),
+					  typename MatrixTraits<Matrix2>::MatrixCategory (),
+					  typename MatrixTraits<Matrix3>::MatrixCategory ()); }
+
+	template <class Matrix1, class Matrix2>
+	inline Matrix2 &trsm (const bool &a, const Matrix1 &A, Matrix2 &B) const
+		{ return trsmSpecialized (a, A, B, typename MatrixTraits<Matrix1>::MatrixCategory (), typename MatrixTraits<Matrix2>::MatrixCategory ()); }
+
+	template <class Matrix1, class Blackbox, class Matrix2>
+	inline Matrix2 &gebmm (Matrix2 &C, const Blackbox &A, const Matrix1 &B) const;
+
+	template <class Matrix1, class Matrix2, class Blackbox>
+	inline Matrix2 &gembm (Matrix2 &C, const Matrix2 &A, const Blackbox &B) const;
+
+	template <class Matrix, class Iterator>
+	inline Matrix &permuteRows (Matrix   &A,
+				    Iterator  P_start,
+				    Iterator  P_end) const
+		{ return permuteRowsSpecialized (A, P_start, P_end,
+						 typename MatrixTraits<Matrix>::MatrixCategory ()); }
+
+	template <class Matrix, class Iterator>
+	inline Matrix &permuteColumns (Matrix   &A,
+				       Iterator  P_start,
+				       Iterator  P_end) const
+		{ return permuteColsSpecialized (A, P_start, P_end,
+						 typename MatrixTraits<Matrix>::MatrixCategory ()); }
+
+private:
+	// Private scratch-pad. It must never contain state, and even
+	// const-methods are free to modify it.
+	DenseZeroOneMatrix<> _Cp;
+
+	// Specialized function implementations
+	template <class Vector1, class Matrix, class Vector2>
+	Vector2 &gemvColDense (const bool &a, const Matrix &A, const Vector1 &x, const bool &b, Vector2 &y) const;
+
+	template <class Vector1, class Matrix, class Vector2>
+	Vector2 &gemvRowSpecialized (const bool &a, const Matrix &A, const Vector1 &x, const bool &b, Vector2 &y,
+				     VectorCategories::DenseZeroOneVectorTag) const;
+	template <class Vector1, class Matrix, class Vector2>
+	Vector2 &gemvRowSpecialized (const bool &a, const Matrix &A, const Vector1 &x, const bool &b, Vector2 &y,
+				     VectorCategories::SparseZeroOneVectorTag) const;
+
+	template <class Vector1, class Matrix, class Vector2>
+	Vector2 &gemvColSpecialized (const bool &a, const Matrix &A, const Vector1 &x, const bool &b, Vector2 &y,
+				     VectorCategories::DenseZeroOneVectorTag,
+				     VectorCategories::DenseZeroOneVectorTag) const
+		{ return gemvColDense (a, A, x, b, y); } 
+	template <class Vector1, class Matrix, class Vector2>
+	Vector2 &gemvColSpecialized (const bool &a, const Matrix &A, const Vector1 &x, const bool &b, Vector2 &y,
+				     VectorCategories::DenseZeroOneVectorTag,
+				     VectorCategories::SparseZeroOneVectorTag) const;
+
+	template <class Vector1, class Matrix, class Vector2>
+	Vector2 &gemvSpecialized (const bool &a, const Matrix &A, const Vector1 &x, const bool &b, Vector2 &y,
+				  MatrixCategories::RowMatrixTag) const
+		{ return gemvRowSpecialized (a, A, x, b, y, typename VectorTraits<Vector1>::VectorCategory ()); }
+	template <class Vector1, class Matrix, class Vector2>
+	Vector2 &gemvSpecialized (const bool &a, const Matrix &A, const Vector1 &x, const bool &b, Vector2 &y,
+				  MatrixCategories::ColMatrixTag) const
+		{ return gemvColSpecialized (a, A, x, b, y,
+					    typename VectorTraits<Vector1>::VectorCategory (),
+					    typename VectorTraits<Vector2>::VectorCategory ()); }
+	template <class Vector1, class Matrix, class Vector2>
+	Vector2 &gemvSpecialized (const bool &a, const Matrix &A, const Vector1 &x, const bool &b, Vector2 &y,
+				  MatrixCategories::ZeroOneRowMatrixTag) const
+		{ return gemvRowSpecialized (a, A, x, b, y, typename VectorTraits<Vector1>::VectorCategory ()); }
+
+	template <class Matrix, class Vector>
+	Vector &trsvSpecialized (const Matrix &A, Vector &x, MatrixCategories::RowMatrixTag, VectorCategories::DenseZeroOneVectorTag);
+
+	template <class Matrix1, class Matrix2> Matrix1 &copyRow (Matrix1 &B, const Matrix2 &A) const;
+	template <class Matrix1, class Matrix2> Matrix1 &copyCol (Matrix1 &B, const Matrix2 &A) const;
+
+	template <class Matrix1, class Matrix2>
+	inline Matrix1 &copySpecialized (Matrix1 &B, const Matrix2 &A,
+					 MatrixCategories::RowMatrixTag,
+					 MatrixCategories::RowMatrixTag) const
+		{ return copyRow (B, A); }
+	template <class Matrix1, class Matrix2>
+	inline Matrix1 &copySpecialized (Matrix1 &B, const Matrix2 &A,
+					 MatrixCategories::ColMatrixTag,
+					 MatrixCategories::ColMatrixTag) const
+		{ return copyCol (B, A); }
+	template <class Matrix1, class Matrix2>
+	inline Matrix1 &copySpecialized (Matrix1 &B, const Matrix2 &A,
+					 MatrixCategories::ZeroOneRowMatrixTag,
+					 MatrixCategories::ZeroOneRowMatrixTag) const
+		{ return copyRow (B, A); }
+
+	template <class Matrix1, class Matrix2> bool areEqualRow (const Matrix1 &A, const Matrix2 &B) const;
+	template <class Matrix1, class Matrix2> bool areEqualCol (const Matrix1 &A, const Matrix2 &B) const;
+
+	template <class Matrix1, class Matrix2>
+	inline bool areEqualSpecialized (const Matrix1 &A, const Matrix2 &B,
+					 MatrixCategories::RowMatrixTag,
+					 MatrixCategories::RowMatrixTag) const
+		{ return areEqualRow (A, B); }
+	template <class Matrix1, class Matrix2>
+	inline bool areEqualSpecialized (const Matrix1 &A, const Matrix2 &B,
+					 MatrixCategories::ColMatrixTag,
+					 MatrixCategories::ColMatrixTag) const
+		{ return areEqualCol (A, B); }
+	template <class Matrix1, class Matrix2>
+	inline bool areEqualSpecialized (const Matrix1 &A, const Matrix2 &B,
+					 MatrixCategories::ZeroOneRowMatrixTag,
+					 MatrixCategories::ZeroOneRowMatrixTag) const
+		{ return areEqualRow (A, B); }
+
+	template <class Matrix> bool isZeroRow (const Matrix &v) const;
+	template <class Matrix> bool isZeroCol (const Matrix &v) const;
+
+	template <class Matrix>
+	bool isZeroSpecialized (const Matrix &A, MatrixCategories::RowMatrixTag) const
+		{ return isZeroRow (A); }
+	template <class Matrix>
+	bool isZeroSpecialized (const Matrix &A, MatrixCategories::ColMatrixTag) const
+		{ return isZeroCol (A); }
+	template <class Matrix>
+	bool isZeroSpecialized (const Matrix &A, MatrixCategories::ZeroOneRowMatrixTag) const
+		{ return isZeroRow (A); }
+
+	template <class Matrix> Matrix &scalRow (Matrix &A, const bool &a) const;
+	template <class Matrix> Matrix &scalCol (Matrix &A, const bool &a) const;
+
+	template <class Matrix>
+	Matrix &scalSpecialized (Matrix &A, const bool &a,
+				 MatrixCategories::RowMatrixTag) const
+		{ return scalRow (A, a); }
+	template <class Matrix>
+	Matrix &scalSpecialized (Matrix &A, const bool &a,
+				 MatrixCategories::ColMatrixTag) const
+		{ return scalCol (A, a); }
+	template <class Matrix>
+	Matrix &scalSpecialized (Matrix &A, const bool &a,
+				 MatrixCategories::ZeroOneRowMatrixTag) const
+		{ return scalRow (A, a); }
+
+	template <class Matrix1, class Matrix2>
+	inline Matrix2 &axpyRow (const bool &a, const Matrix1 &A, Matrix2 &B) const;
+	template <class Matrix1, class Matrix2>
+	inline Matrix2 &axpyCol (const bool &a, const Matrix1 &A, Matrix2 &B) const;
+
+	template <class Matrix1, class Matrix2>
+	inline Matrix2 &axpySpecialized (const bool &a, const Matrix1 &A, Matrix2 &B,
+					 MatrixCategories::RowMatrixTag,
+					 MatrixCategories::RowMatrixTag) const
+		{ return axpyRow (a, A, B); }
+
+	template <class Matrix1, class Matrix2>
+	inline Matrix2 &axpySpecialized (const bool &a, const Matrix1 &A, Matrix2 &B,
+					 MatrixCategories::ZeroOneRowMatrixTag,
+					 MatrixCategories::RowMatrixTag) const
+		{ return axpyRow (a, A, B); }
+
+	template <class Matrix1, class Matrix2>
+	inline Matrix2 &axpySpecialized (const bool &a, const Matrix1 &A, Matrix2 &B,
+					 MatrixCategories::ColMatrixTag,
+					 MatrixCategories::ColMatrixTag) const
+		{ return axpyCol (a, A, B); }
+
+	template <class Matrix1, class Matrix2, class Matrix3>
+	Matrix3 &gemmRowRowCol (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C) const;
+	template <class Matrix1, class Matrix2, class Matrix3>
+	Matrix3 &gemmColRowCol (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C) const;
+	template <class Matrix1, class Matrix2, class Matrix3>
+	Matrix3 &gemmRowRowRow (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C) const
+		{ return gemmRowRowRowSpecialised (a, A, B, b, C, typename VectorTraits<typename Matrix1::ConstRow>::VectorCategory ()); }
+	template <class Matrix1, class Matrix2, class Matrix3>
+	Matrix3 &gemmColColCol (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C) const;
+
+	template <class Matrix1, class Matrix2, class Matrix3>
+	Matrix3 &gemmRowRowRowSpecialised (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C,
+					   VectorCategories::DenseZeroOneVectorTag) const;
+	template <class Matrix1, class Matrix2, class Matrix3>
+	Matrix3 &gemmRowRowRowSpecialised (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C,
+					   VectorCategories::SparseZeroOneVectorTag) const;
+	template <class Matrix1, class Matrix3>
+	Matrix3 &gemmRowRowRow (const bool &a, const Matrix1 &A, const Submatrix<DenseZeroOneMatrix<> > &B, const bool &b, Matrix3 &C) const;
+
+	template <class Matrix1, class Matrix2, class Matrix3>
+	Matrix3 &gemmSpecialized (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C,
+				  MatrixCategories::RowMatrixTag,
+				  MatrixCategories::RowMatrixTag,
+				  MatrixCategories::ColMatrixTag) const
+		{ return gemmRowRowCol (a, A, B, b, C); }
+	template <class Matrix1, class Matrix2, class Matrix3>
+	Matrix3 &gemmSpecialized (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C,
+				  MatrixCategories::ColMatrixTag,
+				  MatrixCategories::RowMatrixTag,
+				  MatrixCategories::ColMatrixTag) const
+		{ return gemmColRowCol (a, A, B, b, C); }
+	template <class Matrix1, class Matrix2, class Matrix3>
+	Matrix3 &gemmSpecialized (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C,
+				  MatrixCategories::ZeroOneRowMatrixTag,
+				  MatrixCategories::RowMatrixTag,
+				  MatrixCategories::ColMatrixTag) const
+		{ return gemmRowRowCol (a, A, B, b, C); }
+	template <class Matrix1, class Matrix2, class Matrix3>
+	Matrix3 &gemmSpecialized (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C,
+				  MatrixCategories::RowMatrixTag,
+				  MatrixCategories::RowMatrixTag,
+				  MatrixCategories::RowMatrixTag) const
+		{ return gemmRowRowRow (a, A, B, b, C); }
+	template <class Matrix1, class Matrix2, class Matrix3>
+	Matrix3 &gemmSpecialized (bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C,
+				  MatrixCategories::ColMatrixTag,
+				  MatrixCategories::ColMatrixTag,
+				  MatrixCategories::ColMatrixTag) const
+		{ return gemmColColCol (a, A, B, b, C); }
+	template <class Matrix1, class Matrix2, class Matrix3>
+	Matrix3 &gemmSpecialized (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C,
+				  MatrixCategories::ZeroOneRowMatrixTag,
+				  MatrixCategories::ZeroOneRowMatrixTag,
+				  MatrixCategories::ZeroOneRowMatrixTag) const
+		{ return gemmRowRowRow (a, A, B, b, C); }
+
+	template <class Matrix1, class Matrix2>
+	Matrix2 &trsmSpecialized (const bool &a, const Matrix1 &A, Matrix2 &B, MatrixCategories::RowMatrixTag, MatrixCategories::RowMatrixTag);
+
+	template <class Matrix1, class Matrix2>
+	Matrix2 &trsmSpecialized (const bool &a, const Matrix1 &A, Matrix2 &B, MatrixCategories::RowMatrixTag, MatrixCategories::ColMatrixTag);
+
+	template <class Matrix, class Iterator>
+	inline Matrix &permuteRowsByRow (Matrix   &A,
+					 Iterator  P_start,
+					 Iterator  P_end) const;
+
+	template <class Matrix, class Iterator>
+	inline Matrix &permuteRowsByCol (Matrix   &A,
+					 Iterator  P_start,
+					 Iterator  P_end) const;
+
+	template <class Matrix, class Iterator>
+	inline Matrix &permuteRowsSpecialized (Matrix   &A,
+					       Iterator  P_start,
+					       Iterator  P_end,
+					       MatrixCategories::ZeroOneRowMatrixTag) const
+		{ return permuteRowsByRow (A, P_start, P_end); }
+
+	template <class Matrix, class Iterator>
+	inline Matrix &permuteRowsSpecialized (Matrix   &A,
+					       Iterator  P_start,
+					       Iterator  P_end,
+					       MatrixCategories::RowMatrixTag) const
+		{ return permuteRowsByRow (A, P_start, P_end); }
+
+	template <class Matrix, class Iterator>
+	inline Matrix &permuteRowsSpecialized (Matrix   &A,
+					       Iterator  P_start,
+					       Iterator  P_end,
+					       MatrixCategories::RowColMatrixTag) const
+		{ return permuteRowsByRow (A, P_start, P_end); }
+
+	template <class Matrix, class Iterator>
+	inline Matrix &permuteRowsSpecialized (Matrix   &A,
+					       Iterator  P_start,
+					       Iterator  P_end,
+					       MatrixCategories::ColMatrixTag) const
+		{ return permuteRowsByCol (A, P_start, P_end); }
+
+	template <class Matrix, class Iterator>
+	inline Matrix &permuteColsByRow (Matrix   &A,
+					 Iterator  P_start,
+					 Iterator  P_end) const;
+
+	template <class Matrix, class Iterator>
+	inline Matrix &permuteColsByCol (Matrix   &A,
+					 Iterator  P_start,
+					 Iterator  P_end) const;
+
+	template <class Matrix, class Iterator>
+	inline Matrix &permuteColsSpecialized (Matrix   &A,
+					       Iterator  P_start,
+					       Iterator  P_end,
+					       MatrixCategories::ZeroOneRowMatrixTag) const
+		{ return permuteColsByRow (A, P_start, P_end); }
+
+	template <class Matrix, class Iterator>
+	inline Matrix &permuteColsSpecialized (Matrix   &A,
+					       Iterator  P_start,
+					       Iterator  P_end,
+					       MatrixCategories::RowMatrixTag) const
+		{ return permuteColsByRow (A, P_start, P_end); }
+
+	template <class Matrix, class Iterator>
+	inline Matrix &permuteColsSpecialized (Matrix   &A,
+					       Iterator  P_start,
+					       Iterator  P_end,
+					       MatrixCategories::ColMatrixTag) const
+		{ return permuteColsByCol (A, P_start, P_end); }
+};
+
+template <>
+class MatrixDomainSupport<GF2> : public MatrixDomainSupportGF2
+{
+    public:
+	MatrixDomainSupport (const GF2 &F) : MatrixDomainSupportGF2 (F) {}
+};
+
+} // namespace LinBox
+
+#include "linbox/matrix/matrix-domain-gf2.inl"
+
+#endif // __MATRIX_MATRIX_DOMAIN_GF2_H
---
+++ linbox/matrix/matrix-domain-gf2.inl
@@ -0,0 +1,364 @@
+/* -*- mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+
+/* linbox/field/matrix-domain-gf2.inl
+ * Copyright 2010 Bradford Hovinen
+ *
+ * Written by Bradford Hovinen <hovinen@gmail.com>
+ *
+ * ------------------------------------
+ *
+ * See COPYING for license information.
+ */
+
+#ifndef __MATRIX_MATRIX_DOMAIN_GF2_INL
+#define __MATRIX_MATRIX_DOMAIN_GF2_INL
+
+#include "linbox/util/error.h"
+
+namespace LinBox
+{
+
+template <class Matrix1, class Matrix2>
+Matrix1 &MatrixDomainSupportGF2::copyRow (Matrix1 &A, const Matrix2 &B) const
+{
+	linbox_check (A.rowdim () == B.rowdim ());
+	linbox_check (A.coldim () == B.coldim ());
+
+	typename Matrix1::RowIterator i;
+	typename Matrix2::ConstRowIterator j;
+
+	i = A.rowBegin ();
+	j = B.rowBegin ();
+
+	for (; i != A.rowEnd (); ++i, ++j)
+		_VD.copy (*i, *j);
+
+	return A;
+}
+
+template <class Matrix1, class Matrix2>
+inline bool MatrixDomainSupportGF2::areEqualRow (const Matrix1 &A, const Matrix2 &B) const
+{
+	linbox_check (A.rowdim () == B.rowdim ());
+	linbox_check (A.coldim () == B.coldim ());
+
+	typename Matrix1::ConstRowIterator i;
+	typename Matrix2::ConstRowIterator j;
+
+	i = A.rowBegin ();
+	j = B.rowBegin ();
+
+	for (; i != A.rowEnd (); ++i, ++j)
+		if (!_VD.areEqual (*i, *j))
+			return false;
+
+	return true;
+}
+
+template <class Matrix>
+bool MatrixDomainSupportGF2::isZeroRow (const Matrix &A) const
+{
+	typename Matrix::ConstRowIterator i;
+
+	i = A.rowBegin ();
+
+	for (; i != A.rowEnd (); ++i)
+		if (!_VD.isZero (*i))
+			return false;
+
+	return true;
+}
+
+template <class Matrix>
+Matrix &MatrixDomainSupportGF2::scalRow (Matrix &A, const bool &a) const
+{
+	if (a)
+		return A;
+	else {
+		typename Matrix::RowIterator i;
+
+		for (i = A.rowBegin (); i != A.rowEnd (); ++i)
+			_VD.mulin (*i, a);
+	}
+}
+
+template <class Matrix1, class Matrix2>
+Matrix2 &MatrixDomainSupportGF2::axpyRow (const bool &a, const Matrix1 &A, Matrix2 &B) const
+{
+	linbox_check (A.rowdim () == B.rowdim ());
+	linbox_check (A.coldim () == B.coldim ());
+
+	typename Matrix1::ConstRowIterator i;
+	typename Matrix2::RowIterator j;
+
+	i = A.rowBegin ();
+	j = B.rowBegin ();
+
+	for (; i != A.rowEnd (); ++i, ++j)
+		_VD.axpyin (*j, a, *i);
+
+	return B;
+}
+
+template <class Vector1, class Matrix, class Vector2>
+Vector2 &MatrixDomainSupportGF2::gemvColDense (const bool &a, const Matrix &A, const Vector1 &x, const bool &b, Vector2 &y) const
+{
+	typename Matrix::ColIterator i_A;
+	typename Vector1::const_iterator i_x;
+
+	if (!b)
+		_VD.subin (y, y);
+
+	if (!a)
+		return y;
+
+	for (i_x = x.begin (), i_A = A.colBegin (); i_x != x.end (); ++i_x, ++i_A)
+		if (*i_x)
+			_VD.addin (y, *i_A);
+
+	return y;
+}
+
+template <class Vector1, class Matrix, class Vector2>
+Vector2 &MatrixDomainSupportGF2::gemvRowSpecialized (const bool &a, const Matrix &A, const Vector1 &x, const bool &b, Vector2 &y,
+						     VectorCategories::DenseZeroOneVectorTag) const
+{
+	typename Matrix::ConstRowIterator i_A;
+	size_t idx;
+
+	bool d;
+
+	if (!b)
+		_VD.subin (y, y);
+
+	if (!a)
+		return y;
+
+	for (i_A = A.rowBegin (), idx = 0; i_A != A.rowEnd (); ++i_A, ++idx) {
+		_VD.dot (d, *i_A, x);
+		_F.addin (y[idx], d);
+	}
+		
+	return y;
+}
+
+template <class Vector1, class Matrix, class Vector2>
+Vector2 &MatrixDomainSupportGF2::gemvRowSpecialized (const bool &a, const Matrix &A, const Vector1 &x, const bool &b, Vector2 &y,
+						     VectorCategories::SparseZeroOneVectorTag) const
+{
+	typename Matrix::ConstRowIterator i_A;
+	size_t idx;
+
+	bool d;
+
+	Vector2 t;
+
+	if (!b)
+		y.clear ();
+
+	if (!a)
+		return y;
+
+	for (i_A = A.rowBegin (), idx = 0; i_A != A.rowEnd (); ++i_A, ++idx) {
+		_VD.dot (d, *i_A, x);
+
+		if (d)
+			t.push_back (idx);
+	}
+
+	_VD.addin (y, t);
+
+	return y;
+}
+
+template <class Matrix, class Vector>
+Vector &MatrixDomainSupportGF2::trsvSpecialized (const Matrix &A, Vector &x,
+						 MatrixCategories::RowMatrixTag,
+						 VectorCategories::DenseZeroOneVectorTag)
+{
+	linbox_check (A.coldim () == A.rowdim ());
+	linbox_check (A.rowdim () == x.size ());
+
+	bool ai;
+	int i = A.rowdim () - 1;
+
+	while (--i >= 0) {
+		if (_VD.firstNonzeroEntry (ai, *(A.rowBegin () + i)) == -1)
+			continue;
+
+		_VD.dot (x[i], *(A.rowBegin () + i), x);
+	}
+
+	return x;
+}
+
+template <class Matrix1, class Matrix2, class Matrix3>
+Matrix3 &MatrixDomainSupportGF2::gemmRowRowRowSpecialised (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C,
+							   VectorCategories::DenseZeroOneVectorTag) const
+{
+	linbox_check (A.coldim () == B.rowdim ());
+	linbox_check (A.rowdim () == C.rowdim ());
+	linbox_check (B.coldim () == C.coldim ());
+
+	typename Matrix1::ConstRowIterator i;
+	typename Matrix2::ConstRowIterator j;
+	typename Matrix3::RowIterator k;
+	typename Matrix1::ConstRow::const_iterator ip;
+
+	for (i = A.rowBegin (), k = C.rowBegin (); i != A.rowEnd (); ++i, ++k) {
+		if (!b)
+			_VD.mulin (*k, false);
+
+		if (a)
+			for (j = B.rowBegin (), ip = i->begin (); j != B.rowEnd (); ++j, ++ip)
+				if (*ip)
+					_VD.addin (*k, *j);
+	}
+
+	return C;
+}
+
+template <class Matrix1, class Matrix3>
+Matrix3 &MatrixDomainSupportGF2::gemmRowRowRow (const bool &a, const Matrix1 &A, const Submatrix<DenseZeroOneMatrix<> > &B, const bool &b, Matrix3 &C) const
+{
+	linbox_check (A.coldim () == B.rowdim ());
+	linbox_check (A.rowdim () == C.rowdim ());
+	linbox_check (B.coldim () == C.coldim ());
+
+	if (!a) {
+		if (!b)
+			scal (C, false);
+
+		return C;
+	}
+
+	size_t raw_coldim = (B.startCol () & __LINBOX_POS_ALL_ONES) + B.coldim ();
+	size_t coldim = (!(raw_coldim & __LINBOX_POS_ALL_ONES)) ? raw_coldim : ((raw_coldim & ~__LINBOX_POS_ALL_ONES) + __LINBOX_BITSOF_LONG);
+
+	DenseZeroOneMatrix<> &Cp = *const_cast<DenseZeroOneMatrix<> *> (&_Cp);
+	Cp.resize (C.rowdim (), coldim);
+
+	DenseZeroOneMatrix<BitVector::const_word_iterator, BitVector::const_word_iterator> Bp (B.parent (), B.startRow (), B.startCol () & ~__LINBOX_POS_ALL_ONES, B.rowdim (), coldim);
+
+	gemm (a, A, Bp, b, Cp);
+
+	Submatrix<DenseZeroOneMatrix<> > Cpp (Cp, 0, B.startCol () & __LINBOX_POS_ALL_ONES, C.rowdim (), B.coldim ());
+	copy (C, Cpp);
+
+	return C;
+}
+
+template <class Matrix1, class Matrix2, class Matrix3>
+Matrix3 &MatrixDomainSupportGF2::gemmRowRowRowSpecialised (const bool &a, const Matrix1 &A, const Matrix2 &B, const bool &b, Matrix3 &C,
+							   VectorCategories::SparseZeroOneVectorTag) const
+{
+	linbox_check (A.coldim () == B.rowdim ());
+	linbox_check (A.rowdim () == C.rowdim ());
+	linbox_check (B.coldim () == C.coldim ());
+
+	typename Matrix1::ConstRowIterator i;
+	typename Matrix2::ConstRowIterator j;
+	typename Matrix3::RowIterator k;
+	typename Matrix1::ConstRow::const_iterator ip;
+
+	for (i = A.rowBegin (), k = C.rowBegin (); i != A.rowEnd (); ++i, ++k) {
+		if (_F.isZero (b))
+			_VD.mulin (*k, false);
+
+		if (_F.isOne (a))
+			for (ip = i->begin (); ip != i->end (); ++ip)
+				_VD.addin (*k, *(B.rowBegin () + *ip));
+	}
+
+	return C;
+}
+
+template <class Matrix1, class Matrix2>
+Matrix2 &MatrixDomainSupportGF2::trsmSpecialized (const bool &a, const Matrix1 &A, Matrix2 &B,
+						  MatrixCategories::RowMatrixTag, MatrixCategories::RowMatrixTag)
+{
+	linbox_check (A.coldim () == A.rowdim ());
+	linbox_check (A.rowdim () == B.rowdim ());
+
+	bool ai;
+	int i = A.rowdim () - 1;
+
+	TransposeMatrix<const Matrix1> AT (A);
+
+	while (--i >= 0) {
+		if (_VD.firstNonzeroEntry (ai, *(A.rowBegin () + i)) == -1)
+			continue;
+
+		gemv (true, AT, *(A.rowBegin () + i), false, *(B.rowBegin () + i));
+	}
+
+	return B;
+}
+
+template <class Matrix1, class Matrix2>
+Matrix2 &MatrixDomainSupportGF2::trsmSpecialized (const bool &a, const Matrix1 &A, Matrix2 &B,
+						  MatrixCategories::RowMatrixTag, MatrixCategories::ColMatrixTag)
+{
+	typename Matrix2::ColIterator i_B;
+
+	if (!a) {
+		subin (B, B);
+		return B;
+	}
+
+	for (i_B = B.colBegin (); i_B != B.colEnd (); ++i_B)
+		trsv (A, *i_B);
+
+	return B;
+}
+
+template <class Matrix, class Iterator>
+inline Matrix &MatrixDomainSupportGF2::permuteRowsByRow (Matrix   &A,
+							 Iterator  P_start,
+							 Iterator  P_end) const
+{
+	Iterator i;
+	typename Matrix::RowIterator j, k;
+
+	for (i = P_start; i != P_end; ++i) {
+		j = A.rowBegin () + i->first;
+		k = A.rowBegin () + i->second;
+
+		_VD.swap (*j, *k);
+	}
+
+	return A;
+}
+
+template <class Matrix, class Iterator>
+Matrix &MatrixDomainSupportGF2::permuteRowsByCol (Matrix   &A,
+						  Iterator  P_start,
+						  Iterator  P_end) const
+{
+	throw LinboxNotImplemented (__FUNCTION__);
+
+	typename Matrix::ColIterator j;
+
+//	for (j = A.colBegin (); j != A.colEnd (); ++j)
+//		_VD.permute (*j, P_start, P_end);
+
+	return A;
+}
+
+template <class Matrix, class Iterator>
+Matrix &MatrixDomainSupportGF2::permuteColsByRow (Matrix   &A,
+						  Iterator  P_start,
+						  Iterator  P_end) const
+{
+	typename Matrix::RowIterator j;
+
+	for (j = A.rowBegin (); j != A.rowEnd (); ++j)
+		_VD.permute (*j, P_start, P_end);
+
+	return A;
+}
+
+}
+
+#endif // __MATRIX_MATRIX_DOMAIN_GF2_INL
+
