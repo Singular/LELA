c5f391c3cb8345f356b5ad16bb85e516f8b1dafb
Bradford Hovinen <hovinen@gmail.com>
2011-05-09 12:17:56 +0200

    * Fold contents of dense.inl into dense.h and remove dense.inl * Make DenseMatrixRowIterator and DenseMatrixColIterator private member-classes of DenseMatrix * Move MatrixCategory-definition into DenseMatrix

--- linbox/matrix/dense.h
+++ linbox/matrix/dense.h
@@ -1,31 +1,14 @@
 /* linbox/matrix/dense.h
- * Copyright (C) 2001 B. David Saunders,
- *               2001-2002 Bradford Hovinen,
- *               2002 Zhendong Wan
+ * Copyright 2001 B. David Saunders,
+ *           2001-2002 Bradford Hovinen,
+ *           2002 Zhendong Wan
  *
  * Written by B. David Saunders <saunders@cis.udel.edu>,
- *            Bradford Hovinen <hovinen@cis.udel.edu>,
+ *            Bradford Hovinen <hovinen@gmail.com>,
  *            Zhendong Wan <wan@mail.eecis.udel.edu>
  *
  * evolved from dense-matrix.h by -bds, Zhendong Wan
  *
- * --------------------------------------------------------
- * 2003-01-11  Bradford Hovinen  <bghovinen@math.uwaterloo.ca>
- *
- * Move from blackbox/dense-base.h to matrix/dense.h
- * --------------------------------------------------------
- * 2002-11-29  Bradford Hovinen  <bghovinen@math.uwaterloo.ca>
- *
- * Swap the order of arguments in read and write, so that it is consistent with
- * SparseMatrixBase
- * --------------------------------------------------------
- * 2002-10-28  Bradford Hovinen  <bghovinen@math.uwaterloo.ca>
- *
- * Rename ColOfRowsIterator as RowIterator; similarly with RowOfColsIterator
- * --------------------------------------------------------
- * 2002-10-27  Bradford Hovinen  <hovinen@cis.udel.edu>
- *
- * Split out container/iterator functionality into DenseMatrixBase
  * --------------------------------------------------------
  *
  * See COPYING for license information
@@ -38,21 +21,17 @@
 #include <vector>
 #include <fstream>
 
+#include "linbox/linbox-config.h"
+#include "linbox/util/debug.h"
 #include "linbox/vector/subiterator.h"
 #include "linbox/vector/subvector.h"
 #include "linbox/vector/stream.h"
 #include "linbox/matrix/matrix-domain.h"
 #include "linbox/matrix/raw-iterator.h"
-#include "linbox/linbox-config.h"
+#include "linbox/matrix/submatrix.h"
 
 namespace LinBox
 {
-
-template <class Iterator, class ConstIterator>
-class DenseMatrixRowIterator;
-
-template <class Iterator, class ConstIterator>
-class DenseMatrixColIterator;
 
 /** Blackbox dense matrix template. This is a class of dense matrices
  * templatized by the entry type, the Element type of some {@link Fields field}.
@@ -69,25 +48,252 @@ class DenseMatrixColIterator;
 template <class _Element>
 class DenseMatrix
 {
+	template <class Iterator, class ConstIterator>
+	class RowIteratorPT
+	{
+	public:
+		typedef Subvector<Iterator, ConstIterator> Row;
+		typedef Subvector<ConstIterator, ConstIterator> ConstRow;
+
+		typedef std::random_access_iterator_tag iterator_category;
+		typedef Row value_type;
+		typedef Row &reference;
+		typedef ConstRow &const_reference;
+		typedef Row *pointer;
+		typedef ConstRow *const_pointer;
+		typedef size_t size_type;
+		typedef typename std::iterator_traits<Iterator>::difference_type difference_type;
+
+		RowIteratorPT (const Iterator &p, size_t len, size_t d)
+			: _row (p, p + len), _dis (d) {}
+
+		RowIteratorPT () {}
+
+		RowIteratorPT (const RowIteratorPT &colp)
+			: _row (colp._row), _dis (colp._dis) {}
+
+		template <class It, class CIt>
+		RowIteratorPT (const RowIteratorPT<It, CIt> &colp)
+			: _row (colp._row), _dis (colp._dis) {}
+
+		RowIteratorPT &operator = (const RowIteratorPT &colp)
+		{
+			_row = colp._row;
+			_dis = colp._dis;
+			return *this;
+		}
+
+		template <class It, class CIt>
+		RowIteratorPT &operator = (const RowIteratorPT<It, CIt> &colp)
+		{
+			_row = colp._row;
+			_dis = colp._dis;
+			return *this;
+		}
+    
+		RowIteratorPT &operator ++ ()
+		{
+			_row = Row (_row.begin () + _dis, _row.end () + _dis);
+			return *this;
+		}
+    
+		RowIteratorPT operator ++ (int)
+		{
+			RowIteratorPT tmp (*this);
+			++*this;
+			return tmp;
+		}
+    
+		RowIteratorPT &operator -- ()
+		{
+			_row = Row (_row.begin () - _dis, _row.end () - _dis);
+			return *this;
+		}
+
+		RowIteratorPT operator -- (int)
+		{
+			RowIteratorPT tmp (*this);
+			--*this;
+			return tmp;
+		}
+
+		RowIteratorPT operator + (int i)
+			{ return RowIteratorPT (_row.begin () + _dis * i, _row.size (), _dis); }
+
+		RowIteratorPT &operator += (int i)
+		{
+			_row = Row (_row.begin () + _dis * i, _row.end () + _dis * i);
+			return *this;
+		}
+
+		difference_type operator - (const RowIteratorPT &c) const
+			{ return (_row.begin () - c._row.begin ()) / _dis; }
+
+		template <class It, class CIt>
+		difference_type operator - (const RowIteratorPT<It, CIt> &c) const
+			{ return (_row.begin () - c._row.begin ()) / _dis; }
+
+		Row operator [] (int i) const
+			{ return Row (const_cast<Row&> (_row).begin () + _dis * i,
+				      const_cast<Row&> (_row).end () + _dis * i); }
+
+		Row *operator -> ()
+			{ return &_row; }
+
+		Row &operator * ()
+			{ return _row; }
+
+		bool operator == (const RowIteratorPT &c) const
+			{ return (_row.begin () == c._row.begin ()) && (_row.end () == c._row.end ()) && (_dis == c._dis); }
+
+		template <class It, class CIt>
+		bool operator == (const RowIteratorPT<It, CIt> &c) const
+			{ return (_row.begin () == c._row.begin ()) && (_row.end () == c._row.end ()) && (_dis == c._dis); }
+
+		bool operator != (const RowIteratorPT &c) const
+			{ return (_row.begin () != c._row.begin ()) || (_row.end () != c._row.end ()) || (_dis != c._dis); }
+
+		template <class It, class CIt>
+		bool operator != (const RowIteratorPT<It, CIt> &c) const
+			{ return (_row.begin () != c._row.begin ()) || (_row.end () != c._row.end ()) || (_dis != c._dis); }
+
+	private:
+		Row _row;
+		size_t _dis;
+
+		template <class It, class CIt>
+		friend class RowIteratorPT;
+	};
+
+	template <class Iterator, class ConstIterator>
+	class ColIteratorPT
+	{
+	public:
+		typedef Subvector<Subiterator<Iterator>, Subiterator<ConstIterator> > Col;
+		typedef Subvector<Subiterator<ConstIterator> > ConstCol;
+
+		typedef std::random_access_iterator_tag iterator_category;
+		typedef Col value_type;
+		typedef Col &reference;
+		typedef ConstCol &const_reference;
+		typedef Col *pointer;
+		typedef ConstCol *const_pointer;
+		typedef size_t size_type;
+		typedef typename std::iterator_traits<Iterator>::difference_type difference_type;
+
+		ColIteratorPT (Iterator p, size_t stride, size_t len)
+			: _col (Subiterator<Iterator> (p, stride),
+				Subiterator<Iterator> (p + len * stride, stride)), _stride (stride)
+			{}
+    
+		ColIteratorPT () {}
+    
+		ColIteratorPT (const ColIteratorPT &rowp)
+			: _col (rowp._col), _stride (rowp._stride) {}
+
+		template <class It, class CIt>
+		ColIteratorPT (const ColIteratorPT<It, CIt> &rowp)
+			: _col (rowp._col), _stride (rowp._stride) {}
+
+		ColIteratorPT &operator = (const ColIteratorPT &rowp)
+		{
+			_col = rowp._col;
+			_stride = rowp._stride;
+			return *this;
+		}
+
+		template <class It, class CIt>
+		ColIteratorPT &operator = (const ColIteratorPT<It, CIt> &rowp)
+		{
+			_col = rowp._col;
+			_stride = rowp._stride;
+			return *this;
+		}
+    
+		ColIteratorPT &operator++ ()
+		{
+			_col = Col (Subiterator<Iterator> (_col.begin ().operator-> () + 1, _stride),
+				    Subiterator<Iterator> (_col.end ().operator-> () + 1, _stride));
+			return *this;
+		}
+    
+		ColIteratorPT operator++ (int)
+		{
+			Col tmp (_col);
+			this->operator++ ();
+			return tmp;
+		}
+        
+		ColIteratorPT operator + (int i)
+			{ return ColIteratorPT (_col.begin ().operator-> () + i, _stride, _col.size ()); }
+
+		ColIteratorPT &operator += (int i)
+		{
+			_col = Col (Subiterator<Iterator> (_col.begin ().operator-> () + i, _stride),
+				    Subiterator<Iterator> (_col.end ().operator-> () + i, _stride));
+			return *this;
+		}
+
+		difference_type operator - (const ColIteratorPT &c) const
+			{ return _col.begin () - c._col.begin (); }
+
+		template <class It, class CIt>
+		difference_type operator - (const ColIteratorPT<It, CIt> &c) const
+			{ return _col.begin () - c._col.begin (); }
+
+		Col operator [] (int i) const
+			{ return Col (Subiterator<Iterator> (const_cast<Col&> (_col).begin ().operator-> () + i, _stride), 
+				      Subiterator<Iterator> (const_cast<Col&> (_col).end ().operator-> () + i, _stride)); }
+
+		Col *operator -> ()
+			{ return &_col; }
+
+		Col &operator * ()
+			{ return _col; }
+
+		bool operator == (const ColIteratorPT &c) const
+			{ return (_col.begin () == c._col.begin ()) && (_col.end () == c._col.end ()); }
+
+		template <class It, class CIt>
+		bool operator == (const ColIteratorPT<It, CIt> &c) const
+			{ return (_col.begin () == c._col.begin ()) && (_col.end () == c._col.end ()); }
+
+		bool operator != (const ColIteratorPT &c) const
+			{ return (_col.begin () != c._col.begin ()) || (_col.end () != c._col.end ()); }
+
+		template <class It, class CIt>
+		bool operator != (const ColIteratorPT<It, CIt> &c) const
+			{ return (_col.begin () != c._col.begin ()) || (_col.end () != c._col.end ()); }
+
+	private:
+
+		Col _col;
+		size_t _stride;
+
+		template <class It, class CIt>
+		friend class ColIteratorPT;
+	};
+
     public:
 
 	typedef _Element Element;
 	typedef typename RawVector<Element>::Dense Rep;
         typedef DenseMatrix<_Element> Self_t;
-
-	typedef DenseMatrixRowIterator<typename Rep::iterator, typename Rep::const_iterator> RowIterator;
-	typedef DenseMatrixRowIterator<typename Rep::const_iterator, typename Rep::const_iterator> ConstRowIterator;
+	typedef MatrixCategories::RowColMatrixTag MatrixCategory; 
 
 	typedef Subvector<typename Rep::iterator, typename Rep::const_iterator> Row;
 	typedef Subvector<typename Rep::const_iterator> ConstRow;  
-
-	typedef DenseMatrixColIterator<typename Rep::iterator, typename Rep::const_iterator> ColIterator;
-	typedef DenseMatrixColIterator<typename Rep::const_iterator, typename Rep::const_iterator> ConstColIterator;
 
 	typedef Subvector<Subiterator<typename Rep::iterator> > Col;
 	typedef Subvector<Subiterator<typename Rep::const_iterator> > ConstCol;
 	typedef Col Column;
 	typedef ConstCol ConstColumn;
+
+	typedef RowIteratorPT<typename Rep::iterator, typename Rep::const_iterator> RowIterator;
+	typedef RowIteratorPT<typename Rep::const_iterator, typename Rep::const_iterator> ConstRowIterator;
+
+	typedef ColIteratorPT<typename Rep::iterator, typename Rep::const_iterator> ColIterator;
+	typedef ColIteratorPT<typename Rep::const_iterator, typename Rep::const_iterator> ConstColIterator;
 
 	template<typename _Tp1>
         struct rebind
@@ -135,7 +341,15 @@ class DenseMatrix
 		}
 	}
 
-	DenseMatrix (VectorStream<Row> &vs);
+	DenseMatrix (VectorStream<Row> &vs)
+		: _rep (vs.size () * vs.dim ()), _rows (vs.size ()), _cols (vs.dim ()), _disp (vs.dim ()), _ptr (&_rep[0])
+	{
+		_rep_begin = _rep.begin ();
+		_rep_end = _rep.end ();
+
+		for (RowIterator i = rowBegin (); i != rowEnd (); ++i)
+			vs >> *i;
+	}
 
 	///
 	DenseMatrix &operator = (const DenseMatrix &M)
@@ -214,10 +428,14 @@ class DenseMatrix
 	 * a row vector in dense format
 	 */
 
-	RowIterator rowBegin ();  
-	RowIterator rowEnd ();
-	ConstRowIterator rowBegin () const;        
-	ConstRowIterator rowEnd () const;
+	RowIterator rowBegin ()
+		{ return RowIterator (_rep_begin, _cols, _disp); }
+	RowIterator rowEnd ()
+		{ return RowIterator (_rep_begin + _rows * _disp, _cols, _disp); }
+	ConstRowIterator rowBegin () const
+		{ return ConstRowIterator (_rep_begin, _cols, _disp); }  
+	ConstRowIterator rowEnd () const
+		{ return ConstRowIterator (_rep_begin + _rows * _disp, _cols, _disp); }
 
 	/** @name Row of columns iterator
 	 * The row of columns iterator traverses the columns of the
@@ -225,10 +443,14 @@ class DenseMatrix
 	 * a column vector in dense format
 	 */
 
-	ColIterator colBegin ();
-	ColIterator colEnd ();
-	ConstColIterator colBegin () const;    
-	ConstColIterator colEnd () const;
+	ColIterator colBegin ()
+		{ return ColIterator (_rep_begin, _disp, _rows); }
+	ColIterator colEnd ()
+		{ return ColIterator (_rep_begin + _cols, _disp, _rows); }
+	ConstColIterator colBegin () const
+		{ return ConstColIterator (_rep_begin, _disp, _rows); }
+	ConstColIterator colEnd () const
+		{ return ConstColIterator (_rep_begin + _cols, _disp, _rows); }
 
 	/** \brief
 	 *
@@ -241,10 +463,14 @@ class DenseMatrix
 	typedef typename Rep::iterator RawIterator;
 	typedef typename Rep::const_iterator ConstRawIterator;
     
-	RawIterator rawBegin ();		  
-	RawIterator rawEnd ();
-	ConstRawIterator rawBegin () const;
-	ConstRawIterator rawEnd () const;
+	RawIterator rawBegin ()
+		{ return _rep_begin; }  
+	RawIterator rawEnd ()
+		{ return _rep_end; }
+	ConstRawIterator rawBegin () const
+		{ return _rep_begin; }  
+	ConstRawIterator rawEnd () const
+		{ return _rep_end; }
 
 	/** \brief
 	 *
@@ -290,22 +516,51 @@ class DenseMatrix
 };
 
 template <class Element>
-struct MatrixTraits< DenseMatrix<Element> >
-{ 
-	typedef DenseMatrix<Element> MatrixType;
-	typedef typename MatrixCategories::RowColMatrixTag MatrixCategory; 
+class SubvectorFactory<DenseMatrix<Element> >
+{
+    public:
+	typedef typename DenseMatrix<Element>::Row RowSubvector;
+	typedef typename DenseMatrix<Element>::ConstRow ConstRowSubvector;
+	typedef typename DenseMatrix<Element>::Col ColSubvector;
+	typedef typename DenseMatrix<Element>::ConstCol ConstColSubvector;
+
+	RowSubvector MakeRowSubvector (Submatrix<DenseMatrix<Element> > &M,
+				       typename DenseMatrix<Element>::RowIterator &pos)
+		{ return RowSubvector (pos->begin () + M.startCol (), pos->begin () + (M.startCol () + M.coldim ())); }
+
+	ConstRowSubvector MakeConstRowSubvector (const Submatrix<DenseMatrix<Element> > &M,
+						 typename DenseMatrix<Element>::ConstRowIterator &pos)
+		{ return ConstRowSubvector (pos->begin () + M.startCol (), pos->begin () + (M.startCol () + M.coldim ())); }
+
+	ColSubvector MakeColSubvector (Submatrix<DenseMatrix<Element> > &M,
+				       typename DenseMatrix<Element>::ColIterator &pos)
+		{ return ColSubvector (Subiterator<typename DenseMatrix<Element>::Rep::iterator> (pos->begin ().operator-> (), M.parent ().coldim ()) + M.startRow (),
+				       Subiterator<typename DenseMatrix<Element>::Rep::iterator> (pos->begin ().operator-> (), M.parent ().coldim ()) + (M.startRow () + M.rowdim ())); }
+	ConstColSubvector MakeConstColSubvector (const Submatrix<DenseMatrix<Element> > &M,
+						 typename DenseMatrix<Element>::ConstColIterator &pos)
+		{ return ConstColSubvector (Subiterator<typename DenseMatrix<Element>::Rep::const_iterator> (pos->begin ().operator-> (), M.parent ().coldim ()) + M.startRow (),
+					    Subiterator<typename DenseMatrix<Element>::Rep::const_iterator> (pos->begin ().operator-> (), M.parent ().coldim ()) + (M.startRow () + M.rowdim ())); }
 };
 
 template <class Element>
-struct MatrixTraits< const DenseMatrix<Element> >
-{ 
-	typedef const DenseMatrix<Element> MatrixType;
-	typedef typename MatrixCategories::RowColMatrixTag MatrixCategory; 
+class SubvectorFactory<const DenseMatrix<Element> >
+{
+    public:
+	typedef typename DenseMatrix<Element>::ConstRow RowSubvector;
+	typedef typename DenseMatrix<Element>::ConstRow ConstRowSubvector;
+	typedef typename DenseMatrix<Element>::ConstCol ColSubvector;
+	typedef typename DenseMatrix<Element>::ConstCol ConstColSubvector;
+
+	ConstRowSubvector MakeConstRowSubvector (const Submatrix<const DenseMatrix<Element> > &M,
+						 typename DenseMatrix<Element>::ConstRowIterator &pos)
+		{ return ConstRowSubvector (pos->begin () + M.startCol (), pos->begin () + (M.startCol () + M.coldim ())); }
+	ConstColSubvector MakeConstColSubvector (const Submatrix<const DenseMatrix<Element> > &M,
+						 typename DenseMatrix<Element>::ConstColIterator &pos)
+		{ return ConstColSubvector (Subiterator<typename DenseMatrix<Element>::Rep::const_iterator> (pos->begin ().operator-> (), M.parent ().coldim ()) + M.startRow (),
+					    Subiterator<typename DenseMatrix<Element>::Rep::const_iterator> (pos->begin ().operator-> (), M.parent ().coldim ()) + (M.startRow () + M.rowdim ())); }
 };
 
 } // namespace LinBox
-
-#include "dense.inl"
 
 #endif // __LINBOX_matrix_dense_H
 
