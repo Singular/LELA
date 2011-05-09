27f4be84c9e58308cb2f1ed2fa6f77bb6508b724
Bradford Hovinen <hovinen@gmail.com>
2011-05-09 17:34:23 +0200

    * Fold contents of sparse-subvector-hybrid.inl into sparse-subvector-hybrid.h * HybridSubvectorConstIterator is now a member-class of SparseSubvector<Hybrid>, called const_iterator * Move non-trivial functions in sparse-subvector-hybrid.h into new sparse-subvector-hybrid.tcc

--- linbox/vector/sparse-subvector-hybrid.h
+++ linbox/vector/sparse-subvector-hybrid.h
@@ -21,23 +21,19 @@
 namespace LinBox
 {
 
-template <class Vector>
-class HybridSubvectorConstIterator;
-
 // Specialisation of SparseSubvector to hybrid zero-one format
 
 template <class Vector>
 class SparseSubvector<Vector, VectorCategories::HybridZeroOneVectorTag>
 {
     public:
-	typedef HybridSubvectorConstIterator<Vector> const_iterator;
-
 	typedef typename Vector::index_type index_type;
 	typedef typename Vector::word_type word_type;
 	typedef std::pair<index_type, word_type> value_type;
 	typedef typename Vector::Endianness Endianness;
 
-	friend class HybridSubvectorConstIterator<Vector>;
+	class const_iterator;
+	friend class const_iterator;
 
 	SparseSubvector () {}
 	SparseSubvector (const Vector &v, size_t start, size_t finish)
@@ -49,8 +45,10 @@ class SparseSubvector<Vector, VectorCategories::HybridZeroOneVectorTag>
 
 	~SparseSubvector () {}
 
-	inline const_iterator begin () const;
-	inline const_iterator end   () const;
+	inline const_iterator begin () const
+		{ return const_iterator (*this, _begin); }
+	inline const_iterator end   () const
+		{ return const_iterator (*this, _end_marker); }
 
 	inline size_t         size  () const { return _end_marker - _begin; }
 	inline bool           empty () const { return _end_marker == _begin; }
@@ -60,21 +58,93 @@ class SparseSubvector<Vector, VectorCategories::HybridZeroOneVectorTag>
 	typename Vector::const_iterator _begin, _end, _end_marker;
 	bool _end_is_end;
 
-	void set_start_end (typename Vector::const_iterator begin,
-			    typename Vector::const_iterator end)
-	{
-		_begin = std::lower_bound (begin, end, _start >> WordTraits<word_type>::logof_size, VectorWrapper::CompareSparseEntries ());
-		_end = std::upper_bound (begin, end, _finish >> WordTraits<word_type>::logof_size, VectorWrapper::CompareSparseEntries ());
+	void set_start_end (typename Vector::const_iterator begin, typename Vector::const_iterator end);
 
-		if (_end != _begin && (static_cast<size_t> ((_end - 1)->first) << WordTraits<word_type>::logof_size) + (_start & WordTraits<word_type>::pos_mask) >= _finish)
-			_end_marker = _end - 1;
-		else
-			_end_marker = _end;
-	}
+    public:
+	class const_iterator
+	{
+	public:
+		typedef std::forward_iterator_tag iterator_category;
+		typedef typename Vector::index_type index_type;
+		typedef typename Vector::word_type word_type;
+		typedef typename std::pair<index_type, word_type> value_type;
+		typedef typename std::pair<index_type, word_type> *pointer;
+		typedef typename std::iterator_traits<typename Vector::const_iterator>::difference_type difference_type;
+		typedef typename Vector::Endianness Endianness;
+
+		typedef SparseSubvector<Vector, VectorCategories::HybridZeroOneVectorTag> container_type;
+
+		class const_reference
+		{
+		public:
+			const_reference () {}
+			const_reference (const const_reference &r)
+				: first (r.first), second (r.second) {}
+
+			index_type first;
+			word_type second;
+		};
+
+		typedef const_reference reference;
+
+		const_iterator () {}
+		const_iterator (const container_type &v, const typename Vector::const_iterator &pos)
+			: _v (&v), _pos (pos), _ref_first_valid (false), _ref_second_valid (false), _start (true)
+			{}
+
+		const_iterator (const const_iterator &i)
+			: _v (i._v), _pos (i._pos),
+			  _ref (i._ref), _ref_first_valid (i._ref_first_valid), _ref_second_valid (i._ref_second_valid), _start (i._start) {}
+
+		const_iterator &operator = (const const_iterator &i)
+		{
+			_v = reinterpret_cast<const container_type *> (i._v);
+			_pos = i._pos;
+			_ref.first = i._ref.first;
+			_ref.second = i._ref.second;
+			_ref_first_valid = i._ref_first_valid;
+			_ref_second_valid = i._ref_second_valid;
+			_start = i._start;
+			return *this;
+		}
+
+		const_iterator &operator ++ ();
+
+		const_iterator operator ++ (int) 
+		{
+			const_iterator tmp (*this);
+			++*this;
+			return tmp;
+		}
+
+		difference_type operator - (const_iterator &i) const 
+			{ return _pos - i._pos; }
+
+		const const_reference &operator * ()
+			{ update_ref (); return _ref; }
+
+		const const_reference *operator -> ()
+			{ update_ref (); return &_ref; }
+
+		bool operator == (const const_iterator &c) const 
+			{ return (_pos == c._pos); }
+
+		bool operator != (const const_iterator &c);
+
+	private:
+		const container_type *_v;
+		typename Vector::const_iterator _pos;
+		const_reference _ref;
+		bool _ref_first_valid;
+		bool _ref_second_valid;
+		bool _start;
+
+		void update_ref ();
+	};
 }; // template <class Vector> class SparseSubvector<Vector, HybridZeroOneVectorTag>
 
 } // namespace LinBox
 
-#include "linbox/vector/sparse-subvector-hybrid.inl"
+#include "linbox/vector/sparse-subvector-hybrid.tcc"
 
 #endif // __VECTOR_SPARSE_SUBVECTOR_HYBRID_H
